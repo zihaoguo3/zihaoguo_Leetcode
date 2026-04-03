927b214b49bc32775fa3f1366d191640eb63162f
Zihao Guo <147188626+zihaoguo3@users.noreply.github.com>
2026-04-03 14:18:23 -0400

    Time: 27 ms (96.75%) | Memory: 12.6 MB (17.49%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -13,9 +13,17 @@ class Solution(object):
         for i in range(n-3):
             if i>0 and nums[i]==nums[i-1]:
                 continue
+            if nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target:
+                break
+            if nums[i]+nums[n-1]+nums[n-2]+nums[n-3]<target:
+                continue
 
             for j in range(i+1,n-2):
                 if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                if nums[i]+nums[j]+nums[j+1]+nums[j+2]>target:
+                    break
+                if nums[i]+nums[j]+nums[n-1]+nums[n-2]<target:
                     continue
                 start=j+1
                 end=n-1
