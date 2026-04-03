a792d2fad7bc8e22a25185b4c51246cdf692a5bd
Zihao Guo <147188626+zihaoguo3@users.noreply.github.com>
2026-04-03 14:17:28 -0400

    Time: 23 ms (98.35%) | Memory: 12.4 MB (78.80%) - LeetSync

---
+++ 18-4sum/4sum.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+
+        n=len(nums)
+        ans=[]
+
+        for i in range(n-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            if nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target:
+                break
+            if nums[i]+nums[n-1]+nums[n-2]+nums[n-3]<target:
+                continue
+
+            for j in range(i+1,n-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                if nums[i]+nums[j]+nums[j+1]+nums[j+2]>target:
+                    break
+                if nums[i]+nums[j]+nums[n-1]+nums[n-2]<target:
+                    continue
+                start=j+1
+                end=n-1
+
+                while start<end:
+                    s=nums[i]+nums[j]+nums[start]+nums[end]
+                    if s<target:
+                        start+=1
+                    elif s>target:
+                        end-=1
+                    else:
+                        ans.append([nums[i],nums[j],nums[start],nums[end]])
+                        start+=1
+                        end-=1
+                        while start<end and nums[start]==nums[start-1]:
+                            start+=1
+                        while start<end and nums[end]==nums[end+1]:
+                            end-=1
+        return ans
