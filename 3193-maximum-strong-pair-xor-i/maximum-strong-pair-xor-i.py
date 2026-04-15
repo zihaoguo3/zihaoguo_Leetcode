74f883938afbea4ec8e17105612b4bdff25effce
Zihao Guo <147188626+zihaoguo3@users.noreply.github.com>
2026-04-15 12:57:07 -0400

    Time: 32 ms (89.38%) | Memory: 12.3 MB (93.81%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
@@ -6,14 +6,15 @@ class Solution(object):
         """
         nums.sort()
         max_xor=0
-        n=len(nums)
+
         l=0
 
-        for r in range(n):
-            while abs(nums[r]-nums[l])>min(nums[l],nums[r]):
-                l+=1
-            for i in range(l,r+1):
-                max_xor=max(max_xor,nums[i]^nums[r])
+        n=len(nums)
 
-                    
+        for r, x in enumerate(nums):
+            while abs(nums[l]-nums[r])>min(nums[l],nums[r]):
+                l+=1
+            
+            for i in range(l,r+1):
+                max_xor=max(max_xor,nums[i]^x)
         return max_xor
