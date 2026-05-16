71031efda6f25617d9dd7230949c529b3aa196c2
Zihao Guo <147188626+zihaoguo3@users.noreply.github.com>
2026-05-15 19:56:27 -0500

    Time: 560 ms (17.76%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-WITH ranking_table as(SELECT player_id, event_date, RANK() OVER(PARTITION BY player_id ORDER BY event_date ASC) as ranking FROM Activity)
-SELECT player_id, event_date as first_login
-FROM ranking_table
-WHERE ranking=1
+SELECT player_id, MIN(event_date) as first_login
+FROM Activity
+GROUP BY player_id
